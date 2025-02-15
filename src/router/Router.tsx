c7c2c7a2e8b4852e7aa9bd920aaaede9480e6dd1
DomilsonFirmino <almeidagilbertofirmino@gmail.com>
2025-02-15 16:57:20 +0100

    fix&build: ErrorBoundary

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,18 +2,24 @@ import { createBrowserRouter, RouterProvider } from "react-router"
 import Search from "../components/pages/Search"
 import Fav from "../components/pages/Fav"
 import Home from "../components/pages/Home"
+import ErrorPage from "../components/pages/ErrorPage"
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: < Home/>,
-    },{
-        path: "/search",
+  {
+    children: [
+      {
+        path: "/Dictionary/",
+        element: < Home/>
+      },{
+        path: "/Dictionary/search",
         element: <Search/>
-    },{
-        path: "/fav",
+      },{
+        path: "/Dictionary/fav",
         element: <Fav/>
-    }
+      }
+    ],
+    errorElement: <ErrorPage></ErrorPage>
+  }
 ])
 export default function Router() {  
   return (
