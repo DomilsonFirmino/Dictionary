bd3279f6799055412471637907a374d404bbbd64
DomilsonFirmino <almeidagilbertofirmino@gmail.com>
2025-02-15 17:02:22 +0100

    Build: HashRouter

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router"
+import {createHashRouter, RouterProvider } from "react-router"
 import Search from "../components/pages/Search"
 import Fav from "../components/pages/Fav"
 import Home from "../components/pages/Home"
 import ErrorPage from "../components/pages/ErrorPage"
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     children: [
       {
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
       }
     ],
     errorElement: <ErrorPage></ErrorPage>
-  }
+  } 
 ])
 export default function Router() {  
   return (
