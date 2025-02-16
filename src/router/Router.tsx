cbd8b56d7504c87d4c3208506b41b6ec324c31de
DomilsonFirmino <almeidagilbertofirmino@gmail.com>
2025-02-16 01:30:48 +0100

    Build

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,10 @@
-import {createBrowserRouter, RouterProvider } from "react-router"
+import {createHashRouter, RouterProvider } from "react-router"
 import Search from "../components/pages/Search"
 import Fav from "../components/pages/Fav"
 import Home from "../components/pages/Home"
 import ErrorPage from "../components/pages/ErrorPage"
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/Dictionary",
     element: < Home/>,
